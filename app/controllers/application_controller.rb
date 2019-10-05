819ab275c0384cd93914aa1c13d2424279fda4cb
Malcolm <echelon1089@gmail.com>
2019-10-04 20:48:09 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  config.web_console.whitelisted_ips = '<IP address here>'
+  #config.web_console.whitelisted_ips = '<IP address here>'
 end
