3cae69f94c59b27518c2aec76da026e51315e34e
github username <github email address>
2019-12-24 00:52:22 -0500

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,21 +1,14 @@
 # spec/features/post_spec.rb
  
 require 'rails_helper'
- 
+
 describe 'new post' do
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
   end
-end
 
-require 'rails_helper'
- 
-describe 'new post' do
- 
-  ...
- 
-  it 'renders HTML in the /new template' do
+  it 'renders the HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content('Post Form')
   end
