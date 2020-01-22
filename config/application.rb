3d13b6854678b0c6c963bd2cc77ad3fc07f3b9a1
Chinedu Arinze <c.s.arinze@gmail.com>
2020-01-21 19:21:56 -0500

    update form on new view to be more aesthetic

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module BlogFlash
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.web_console.whitelisted_ips = '192.168.0.28/24'
+    config.web_console.development_only = false
   end
 end
