612f25868f68759bbd17a563cdfffde9ef2447db
bruffin1221 <bruffin1221@gmail.com>
2020-07-02 10:06:34 -0700

    Done.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,6 @@
+class Application < Rails::Application
+  config.web_console.whitelisted_ips = '<IP address here>'
+end
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
